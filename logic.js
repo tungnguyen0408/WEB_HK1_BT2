f79a0da06e0a2efd50109c5babb75416402774f6
Nguyen Tung <tungnguyen0408tnnm@gmail.com>
2024-10-24 22:51:42 +0700

    first commmit 24/10

---
+++ logic.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  };
+  
